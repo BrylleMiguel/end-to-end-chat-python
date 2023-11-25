8024dd47d67c16d73c08607aa3392f210d5b7837
brylle <73713815+BrylleMiguel@users.noreply.github.com>
2023-11-25 05:41:14 -0500

    added chat history

--- lab5.py
+++ lab5.py
@@ -73,11 +73,16 @@ def decrypt_message(encrypted_message, key):
 
 
 # logging/creating file messages
-def log_message(message, hmac, timestamp, user):
+def log_integrity_and_message(message, hmac, timestamp, user):
     with open("integrity_check.txt", "a") as log_file:
         log_file.write(
             f"{timestamp} - HMAC: {hmac.hex()} - User: {user}  - Message: {message}, \n"
         )
+
+
+def log_actual_messages(timestamp, message, user):
+    with open("actual_messages.txt", "a") as file:
+        file.write(f"{timestamp}, {user}: {message}, \n")
 
 
 # logging/creating file for encrypted message
@@ -105,14 +110,21 @@ def start_server():
 
     # RSA key exchange
     server_key = generate_rsa_key()
-    save_rsa_key_to_file(server_key, "server_private_key.pem")
-    save_rsa_key_to_file(server_key.publickey(), "server_public_key.pem")
+    save_rsa_key_to_file(server_key, "pri_server_key.pem")
+    save_rsa_key_to_file(server_key.publickey(), "pub_server_key.pem")
     conn.sendall(server_key.publickey().export_key())
 
     client_key_data = conn.recv(1024)
     client_key = RSA.import_key(client_key_data)
     enc_session_key = encrypt_rsa_key(client_key, key)
     conn.sendall(enc_session_key)
+
+    print("Previous chat:")
+
+    with open("actual_messages.txt", "r") as logfile:
+        messages = logfile.readlines()
+        for message in messages:
+            print(f"{message}")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         server_future = executor.submit(server_send_messages, conn, key, hmac_key)
@@ -135,7 +147,8 @@ def server_send_messages(conn, key, hmac_key):
         conn.sendall(encrypted_message + hmac)
 
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_message(message, hmac, timestamp, "server sends: ")
+        log_actual_messages(timestamp, message, "server")
+        log_integrity_and_message(message, hmac, timestamp, "server sends: ")
         log_encrypted_message(encrypted_message)
 
         if message.lower() == "end chat":
@@ -150,10 +163,10 @@ def server_recieve_messages(conn, key, hmac_key):
 
         if received_hmac == calculate_hmac(data_to_verify, hmac_key):
             decrypted_message = decrypt_message(data_to_verify, key)
-            print(f"received: {decrypted_message}")
+            print(f"client: {decrypted_message}")
 
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_message(
+            log_integrity_and_message(
                 decrypted_message, received_hmac, timestamp, "server receives: "
             )
 
@@ -176,14 +189,21 @@ def start_client():
 
     # RSA key exchange
     client_key = generate_rsa_key()
-    save_rsa_key_to_file(client_key, "client_private_key.pem")
-    save_rsa_key_to_file(client_key.publickey(), "client_public_key.pem")
+    save_rsa_key_to_file(client_key, "pri_client_key.pem")
+    save_rsa_key_to_file(client_key.publickey(), "pub_client_key.pem")
     client_socket.sendall(client_key.publickey().export_key())
 
     server_key_data = client_socket.recv(1024)
     server_key = RSA.import_key(server_key_data)
     enc_session_key = client_socket.recv(1024)
     session_key = decrypt_rsa_key(client_key, enc_session_key)
+
+    print("Previous chat:")
+
+    with open("actual_messages.txt", "r") as logfile:
+        messages = logfile.readlines()
+        for message in messages:
+            print(f"{message}")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         receive_future = executor.submit(
@@ -209,10 +229,10 @@ def client_receive_messages(client_socket, key, hmac_key):
 
         if received_hmac == calculate_hmac(data_to_verify, hmac_key):
             decrypted_message = decrypt_message(data_to_verify, key)
-            print(f"received: {decrypted_message}")
+            print(f"server: {decrypted_message}")
 
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_message(
+            log_integrity_and_message(
                 decrypted_message, received_hmac, timestamp, "client receives: "
             )
 
@@ -242,7 +262,8 @@ def client_send_messages(client_socket, key, hmac_key):
         client_socket.sendall(encrypted_message + hmac)
 
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_message(message, hmac, timestamp, "client sends: ")
+        log_actual_messages(timestamp, message, "client")
+        log_integrity_and_message(message, hmac, timestamp, "client sends: ")
         log_encrypted_message(encrypted_message)
 
         if message.lower() == "end chat":
